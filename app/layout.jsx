ecde0f4f5023fa002e187cab427f2f713f697a74
Mykola Dotsenko <mykola.dotsenko@nidos.fi>
2024-09-20 17:58:14 +0300

    auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,26 @@
-import '@/assets/styles/globals.css';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-
+import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
-    title: 'Property Pulse',
-    keywords: 'rental, property, real estate',
-    description: 'Find the perfect rental property'
-}
-
+  title: "Property Pulse",
+  keywords: "rental, property, real estate",
+  description: "Find the perfect rental property",
+};
 
 const MainLayout = ({ children }) => {
-
-    return (
-        <html>
-            <body>
-
-                <Navbar />
-                <main>
-                    {children}
-                </main>
-                <Footer />
-            </body>
-        </html>
-    )
-}
-
+  return (
+    <AuthProvider>
+      <html>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
+  );
+};
 
 export default MainLayout;
