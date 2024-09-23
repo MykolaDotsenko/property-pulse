6c4c1b1f7160664ce46194ad4908317b37574bfb
Mykola Dotsenko <docnikolaj1990@gmail.com>
2024-09-23 13:36:32 +0300

    Profile Image

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,6 +11,7 @@ import { set } from "mongoose";
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const profileImage = session?.user?.image;
 
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
@@ -164,7 +165,9 @@ const Navbar = () => {
                     <span className="sr-only">Open user menu</span>
                     <Image
                       className="h-8 w-8 rounded-full"
-                      src={profileDefault}
+                      src={profileImage ||profileDefault}
+                      width={40}
+                      height={40}
                       alt=""
                     />
                   </button>
