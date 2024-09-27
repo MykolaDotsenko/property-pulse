e16bd8b84223d62b7569d710d98816bad012026e
Mykola Dotsenko <docnikolaj1990@gmail.com>
2024-09-27 23:59:26 +0300

    map image

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -5,7 +5,10 @@
 async function addProperty(formData) {
     //access all values from amenities
   const amenities = formData.getAll('amenities');
-  const images = formData.getAll('images');
+  const images = formData
+  .getAll('images')
+  .filter((image) => image.name !== '')
+  .map((image) => image.name);
 }
 
 
