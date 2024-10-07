95e764faea6c6a4f18febffd03a618a705cdbeec
Mykola Dotsenko <docnikolaj1990@gmail.com>
2024-10-07 21:01:26 +0300

    Cloudinary Image Upload

--- app/actions/addProperty.js
+++ app/actions/addProperty.js
@@ -4,6 +4,7 @@ import Property from "@/models/Property";
 import { getSessionUser } from "@/utils/getSessionUser";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import cloudinary from "@/config/cloudinary";
 
 
 
@@ -26,7 +27,7 @@ const {userId} = sessionUser;
   const images = formData
   .getAll('images')
   .filter((image) => image.name !== '')
-  .map((image) => image.name);
+  
 
   const propertyData = {
     owner: userId,
@@ -53,8 +54,29 @@ const {userId} = sessionUser;
         email: formData.get('seller_info.email'),
         phone: formData.get('seller_info.phone'),
     },
-    images,
   };
+
+
+const imageUrls = [];
+
+for (const imageFile of images) {
+  const imageBuffer = await imageFile.arrayBuffer();
+  const imageArray = Array.from(new Uint8Array(imageBuffer));
+  const imageData = Buffer.from(imageArray);
+
+  //convert to base64
+  const imageBase64 = imageData.toString('base64');
+
+  //Make request to cloudinary
+  const result = await cloudinary.uploader.upload(`data:image/png;base64,${imageBase64}`, {
+    folder: 'propertypulse',
+  });
+  imageUrls.push(result.secure_url);
+}
+
+
+propertyData.images = imageUrls;
+
   const newProperty = new  Property(propertyData);
   await newProperty.save();
 
