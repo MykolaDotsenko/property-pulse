87165e13a7ce82682192e195e70020a6bb41fac5
Mykola Dotsenko <docnikolaj1990@gmail.com>
2024-12-30 22:23:06 +0200

    Geocode For Latitude & Longitude

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -21,18 +21,18 @@ const PropertyPage = async ({ params }) => {
     <>
       <PropertyHeaderImage image={property.images[0]} />
       <section>
-        <div class="container m-auto py-6 px-6">
+        <div className="container m-auto py-6 px-6">
           <Link
             href="/properties"
-            class="text-blue-500 hover:text-blue-600 flex items-center"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
           >
             <FaArrowLeft className="mr-2" /> Back to Properties
           </Link>
         </div>
       </section>
-      <section class="bg-blue-50">
-        <div class="container m-auto py-10 px-6">
-          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
           <PropertyDetails property={property} />
           </div>
         </div>
