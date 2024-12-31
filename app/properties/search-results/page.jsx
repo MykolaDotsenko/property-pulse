99f589a683276064530cda066edad493dddc208f
Mykola Dotsenko <docnikolaj1990@gmail.com>
2024-12-31 13:58:43 +0200

    Display Search Results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,39 +1,65 @@
+import Link from "next/link";
+import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { convertToSerializableObject } from "@/utils/convertToObject";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
+const SerchResultsPage = async ({
+  searchParams: { location, propertyType },
+}) => {
+  await connectDB();
 
-const SerchResultsPage = async ({ searchParams: { location, propertyType } }) => {
-await connectDB();
+  const locationPattern = new RegExp(location, "i");
 
-const locationPattern = new RegExp(location, "i");
-
-let query = {
+  let query = {
     $or: [
-        { location: locationPattern },
-        { description: locationPattern },
-        {'location.street': locationPattern },
-        {'location.city': locationPattern },
-        {'location.state': locationPattern },
-        {'location.zipcode': locationPattern },
+      { location: locationPattern },
+      { description: locationPattern },
+      { "location.street": locationPattern },
+      { "location.city": locationPattern },
+      { "location.state": locationPattern },
+      { "location.zipcode": locationPattern },
+    ],
+  };
 
-    ]
-};
-
-
-if(propertyType && propertyType !== 'All') {
+  if (propertyType && propertyType !== "All") {
     const typePattern = new RegExp(propertyType, "i");
     query.type = typePattern;
-}
+  }
 
-const propertiesQueryResults = await Property.find(query).lean();
-const properties = convertToSerializableObject(propertiesQueryResults);
+  const propertiesQueryResults = await Property.find(query).lean();
+  const properties = convertToSerializableObject(propertiesQueryResults);
+  console.log(properties);
 
- return (
-   <div>
-     <h1>Search Results Page</h1>
-   </div>
- );
-}
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <Link
+            href="/properties"
+            className="flex items-center text-blue-500 hover: underline mb-3"
+          >
+            <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back to Properties
+          </Link>
+          <h1 className="text-2xl font-bold mb-4">Search Results</h1>
+          {properties.length === 0 ? ( <p>No search results</p>) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {properties.map((property) => (
+                  <PropertyCard key={property._id} property={property} />
+                ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
 
-  export default SerchResultsPage;
+export default SerchResultsPage;
