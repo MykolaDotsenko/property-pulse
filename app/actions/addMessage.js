9f545e5a025343659d6fadc5e138b6242b5e713b
Mykola Dotsenko <docnikolaj1990@gmail.com>
2025-01-04 00:35:36 +0200

    Submit Message & useFormState Hook

--- app/actions/addMessage.js
+++ app/actions/addMessage.js
@@ -1,38 +1,39 @@
-"use server";
-import connectDB from "@/config/database";
-import Message from "@/models/Message";
-import { getSessionUser } from "@/utils/getSessionUser";
+'use server';
+import connectDB from '@/config/database';
+import Message from '@/models/Message';
+import { getSessionUser } from '@/utils/getSessionUser';
+import { revalidatePath } from 'next/cache';
 
-async function addMessage(formData) {
+async function addMessage(previousState, formData) {
   await connectDB();
 
   const sessionUser = await getSessionUser();
 
-  if (!sessionUser || !sessionUser.userId) {
-    throw new Error("User id is required");
+  if (!sessionUser || !sessionUser.user) {
+    return { error: 'You must be logged in to send a message' };
   }
 
   const { userId } = sessionUser;
 
-  const recipient = formData.get("recipient");
+  const recipient = formData.get('recipient');
 
   if (userId === recipient) {
-    return { error: "You cannot send a message to yourself" };
+    return { error: 'You can not send a message to yourself' };
   }
 
-  const newMessage = new Message ({
+  const newMessage = new Message({
     sender: userId,
     recipient,
-    property: formData.get("property"),
-    name: formData.get("name"),
-    email: formData.get("email"),
-    phone: formData.get("phone"),
-    body: formData.get("body"),
+    property: formData.get('property'),
+    name: formData.get('name'),
+    email: formData.get('email'),
+    phone: formData.get('phone'),
+    body: formData.get('body'),
   });
-  
-  await newMessage.save();  
 
-  return {submitted: true};
+  await newMessage.save();
+
+  return { submitted: true };
 }
 
 export default addMessage;
